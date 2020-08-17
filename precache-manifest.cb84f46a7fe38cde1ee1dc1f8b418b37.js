0f293b005e9dc4d8cb4e9da42309ef4ab80f4769
Meenu <meenu@kunneltech.in>
2020-08-17 13:49:29 +0530

    Updates

---
+++ precache-manifest.cb84f46a7fe38cde1ee1dc1f8b418b37.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515f7b186f9f7875cd2ea0bdf2cdb4a7",
+    "url": "/DailyFocus/index.html"
+  },
+  {
+    "revision": "ae3e865ce89e265ef1ea",
+    "url": "/DailyFocus/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "55556ff31b373a7bdab5",
+    "url": "/DailyFocus/static/css/main.f9d588cf.chunk.css"
+  },
+  {
+    "revision": "ae3e865ce89e265ef1ea",
+    "url": "/DailyFocus/static/js/2.59f2d219.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/DailyFocus/static/js/2.59f2d219.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55556ff31b373a7bdab5",
+    "url": "/DailyFocus/static/js/main.6784b801.chunk.js"
+  },
+  {
+    "revision": "c24efd7cb5b3d56c9761",
+    "url": "/DailyFocus/static/js/runtime-main.9ef67cf4.js"
+  },
+  {
+    "revision": "c1f4fbc7f5be3fe9520a744eeb59d6fc",
+    "url": "/DailyFocus/static/media/lerone-pieters-XnBrDrAJaoo-unsplash.c1f4fbc7.jpg"
+  },
+  {
+    "revision": "9c233d755ebd40caf33ea8bf6eb7a97f",
+    "url": "/DailyFocus/static/media/newyork.9c233d75.png"
+  }
+]);
